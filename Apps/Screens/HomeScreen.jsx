2e3b600e8bebda14876e65dc77ac92736039b1fd
Quang Duy <haquangduy322@gamil.com>
2024-04-18 15:15:02 +0700

    đã fix image slider

--- Apps/Screens/HomeScreen.jsx
+++ Apps/Screens/HomeScreen.jsx
@@ -1,38 +1,35 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../components/HomeScreen/Header'
 import Slider from '../../components/HomeScreen/Slider'
-import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import { Firestore, collection, getDocs, getFirestore } from 'firebase/firestore'
 import { app } from '../../firebaseConfig'
 
 export default function HomeScreen() {
     const db = getFirestore(app);
-    //const [sliderList, setSliderList] = useState([]);
-    // useEffect(() => {
-    //      getSliders();
-    // },[])
+    const [sliderList, setSliderList] = useState([]);
+    useEffect(() => {
+        getSliders();
+    }, [])
 
     //used to get slider for home screen 
-    const getSliders=async() => {
-        setSliderList([])
-    const querySnapshot = await getDocs(collection(db, "sliders"));
-    querySnapshot.forEach((doc) => {
-        //doc.data() is never undefined for query doc snapshots
-    setSliderList(sliderList=>[...sliderList,doc.data()])
-    });
+    const getSliders = async () => {
+        setSliderList([]);
+        const querySnapshot = await getDocs(collection(db, "Sliders"));
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, doc.data());
+            setSliderList(sliderList => [...sliderList, doc.data()])
+        });
     }
+
     return (
         <View className="py-8 px-6 bg-white flex-1">
             <Header />
             {/* slider */}
-            <Slider />
+            <Slider sliderList={sliderList} />
+
         </View>
     )
 }
-
-//Sửa nhưng ko muốn đẩy lên web
-//Sửa nhưng ko muốn đẩy lên web 2 test push
-
-//Hà Quydn gduy
 
 //2:13:54 qua phaanf category list ( home screen )
--- components/HomeScreen/Slider.jsx
+++ components/HomeScreen/Slider.jsx
@@ -1,31 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
 import { FlatList } from 'react-native-gesture-handler'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import React from 'react'
 
 //2:08:58
-export default function Slider(sliderList) {
+export default function Slider({ sliderList }) {
   return (
     <View style={styles.slider}>
       <FlatList
         data={sliderList}
         horizontal={true}
-        showsHorizontalScrollIndicatior={false} // 2:13:26
-        renderItem={({item,index})=>(
-            <View>
-                {/*2:11:14 ko hiện được ảnh*/ }
-                <Image
-                    source={require('./../../assets/Image/6505894.jpg')}
-                    //2:12:24
-                    style={{ width: 48, height: 48,}} //mr-3 rounded-lg object-contain 
-                />
-            </View>
+        showsHorizontalScrollIndicator={false} //2:13:26
+        renderItem={({ item, index }) => (
+          <View>
+            {/* Đã fix hiển thị ảnh */}
+            <Image
+              source={{ uri: item?.image }}
+              //2:12:24
+              style={{
+                width: 330, height: 200, marginRight: 12,
+                borderRadius: 8, objectFit: "contain" //ảnh lỗi không borderRadius được sẽ fix sau
+              }}
+            />
+          </View>
         )}
       />
     </View>
   )
 }
 const styles = StyleSheet.create({
-    slider:{
-        // mt-5 2:13:07
-    }
+  slider: {
+    marginTop: 20
+  }
 })
